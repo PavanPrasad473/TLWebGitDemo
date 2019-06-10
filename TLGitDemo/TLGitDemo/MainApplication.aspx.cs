a52e548095fcb101454f64ea2c913a805450438e
PavanPrasad473 <spavanprasad@gmail.com>
2019-06-11 00:48:03 +0530

    commit

---
+++ TLGitDemo/TLGitDemo/MainApplication.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TLGitDemo
+{
+    public partial class MainApplication : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("This is the main application");
+        }
+    }
+}
